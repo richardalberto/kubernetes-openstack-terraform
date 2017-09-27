b230334a6d8332178b02a09b729c282dc22088ab
Richard Gonzalez <richard_gonzalez@ultimatesoftware.com>
2017-09-27 02:23:47 -0400

    Corrected kubectl config file permissions

--- files/master.sh
+++ files/master.sh
@@ -5,7 +5,7 @@ kubeadm init --pod-network-cidr=10.244.0.0/16 --apiserver-bind-port=443 --token 
 # kubectl
 mkdir -p $HOME/.kube
 cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
-chown $(id -u):$(id -g) $HOME/.kube/config
+chown ubuntu:ubuntu $HOME/.kube/config
 
 # flannel
 kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
